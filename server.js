623a76c90203397dbd6067b313eb2ce11af9e62c
welmeet-chat <welmeet1917@gmail.com>
2024-08-15 21:43:55 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+    console.log('New user connected');
+
+    socket.on('message', (message) => {
+        io.emit('message', message);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+});
+
+server.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
+});
